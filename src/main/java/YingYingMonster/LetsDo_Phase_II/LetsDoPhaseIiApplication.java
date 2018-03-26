e8feb21cc3014d25e41bdcaf42ac844b51795cf0
161250103 <1767855130@qq.com>
2018-03-26 08:53:57 +0800

    first commit

---
+++ src/main/java/YingYingMonster/LetsDo_Phase_II/LetsDoPhaseIiApplication.java
@@ -0,0 +1,12 @@
+package YingYingMonster.LetsDo_Phase_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsDoPhaseIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsDoPhaseIiApplication.class, args);
+	}
+}
