f1663bf530ec1535d224dec641fcd1964bed3724
161250050 <598029681@qq.com>
2018-04-22 11:25:45 +0800

    页面修改

--- src/main/java/YingYingMonster/LetsDo_Phase_II/controller/WorkSpaceController.java
+++ src/main/java/YingYingMonster/LetsDo_Phase_II/controller/WorkSpaceController.java
@@ -1,6 +1,7 @@
 package YingYingMonster.LetsDo_Phase_II.controller;
 
 import java.io.OutputStream;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import YingYingMonster.LetsDo_Phase_II.model.Data;
 import YingYingMonster.LetsDo_Phase_II.service.WorkerService;
 
 
@@ -104,10 +106,19 @@ public class WorkSpaceController {
      * 随机获得一个待完成的图片的id
      * @return 图片id
      */
-    @RequestMapping("/getNewPictureId/{userId}/{projectId}")  
+    @RequestMapping("/getNewPictureId/{userId}/{projectId}/{publisherId}")  
     @ResponseBody  
-    public String getNewPictureId() {
-    	String id = "" ; //调用service的方法
+    public String getNewPictureId(@PathVariable("projectId")String projectId ,
+   			@PathVariable("userId")String userId,
+   			@PathVariable("publisherId")String publisherId) {
+    	List<String> list = service.viewUndoData(userId, publisherId, projectId);
+    	
+    	if(list==null||list.size()==0) {
+    		return "";//没有内容
+    	}
+    	
+    	String id = list.get(0) ; //需要检查是否有内容！！
+    	
     	return id;
     }
     
@@ -115,17 +126,23 @@ public class WorkSpaceController {
     /**
    	 * 把一张等待完成的图片放到url里
    	 */
-     @RequestMapping("/getNewPicture/{userId}/{projectId}/{pictureId}")  
+     @RequestMapping("/getNewPicture/{userId}/{projectId}/{publisherId}/{pictureId}")  
      @ResponseBody  
      public void getNewImage(HttpServletRequest request, HttpServletResponse response,@PathVariable("projectId")String projectId ,
    			@PathVariable("userId")String userId,
+   			@PathVariable("publisherId")String publisherId,
    			@PathVariable("pictureId")String pictureId) throws Exception{  
        	
        	String JPG="image/jpeg;charset=UTF-8";      
            // 获取输出流  
            OutputStream outputStream = response.getOutputStream();  
            // 读数据  
-           byte[] data = null;// service.getPicture(pictureId);//从service获得
+           Data dataPac = service.getAData(userId, publisherId, projectId, pictureId);
+           System.out.println(userId+" "+publisherId+" "+projectId+" "+pictureId);
+           byte[] data = dataPac.getData();
+           int height = dataPac.getHeight();
+           int width = dataPac.getWidth();
+           String size = width+","+height;
            // 回写  
            response.setContentType(JPG);  
            outputStream.write(data);  
